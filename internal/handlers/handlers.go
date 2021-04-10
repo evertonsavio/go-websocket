7b8c8d3e365dd8112340fd340a61e5c9f1d06d93
havyx <everluca@outlook.com>
2021-04-10 14:39:11 -0300

    :sparkles: websocket connection

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,18 +3,58 @@ package handlers
 import (
 	"log"
 	"net/http"
+
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
+/*=======================================================================================
+VARS
+=======================================================================================*/
 var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
 	jet.InDevelopmentMode(),
 )
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
-func Home(w http.ResponseWriter, r *http.Request){
+/*=======================================================================================
+FUNCS
+=======================================================================================*/
+/*--------------------------HOME HANDLER: Render home page-----------------------------*/
+func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
 	if err != nil {
 		log.Panicln(err)
+	}
+}
+
+//WsJsonResponse defines the response sent back from websocket
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+//WsEndpoint upgrades connection to websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("CLIENT CONNECTED TO ENDPOINT")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+
+	err = ws.WriteJSON(response)
+
+	if err != nil {
+		log.Println(err)
 	}
 }
 
