bfbf6232f433cd6e21e6a48e325d16dfefd1f565
Havyx <everluca@outlook.com>
2021-06-08 14:29:10 -0300

    :sparkles: hmac working

--- src/evertonsavio/application/main.go
+++ src/evertonsavio/application/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/havyx/golang-websocket/src/evertonsavio/application/services"
 	"log"
 	"net/http"
@@ -8,9 +11,6 @@ import (
 
 func main()  {
 
-	ticket := "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJmNDgxZjc4ZS1kYzkyLTRlZTctOTgxNC1jNWI5OTE3MDNmMzAifQ.VqAUqSbuw0sinyQt8dRCx1IUX9I1KjgSv6h14YUhCYuUORABSOCvq0h4IZL4aB9jwUBY0XxiPIoO4U3VgYHw-Q"
-	ExtractClaims(ticket)
-	
 	mux := routes()
 
 	log.Println("Starting channel listener")
@@ -18,7 +18,40 @@ func main()  {
 
 	log.Println("Starting on port 8080")
 
+// 	token, err := getToken("123123")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(token)
+
+// 	tc, err := verifyToken(token)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(tc)
+
 	_ = http.ListenAndServe(":8080", mux)
 
+}
 
+func getToken(name string) (string, error) {
+	signingKey := []byte("PADOPADOPADOPADO")
+	fmt.Println(signingKey)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": name,
+	})
+	tokenString, err := token.SignedString(signingKey)
+	return tokenString, err
+}
+
+func verifyToken(tokenString string) (jwt.Claims, error) {
+	signingKey := []byte("PADOPADOPADOPADO")
+	fmt.Println(signingKey)
+	testToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjMxMjMifQ.eoqrniuoQeG9hEwV9bJGBT5bFE5vn8CyPgenVZqwYDY"
+
+	token, err := jwt.Parse(testToken, func(token *jwt.Token) (interface{}, error) { return signingKey, nil })
+	if err != nil {
+		return nil, err
+	}
+	return token.Claims, err
 }
